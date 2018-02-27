ff25e8f9f20b8b34c986ce131d2eb377c65f02d0
-ah chu- <2013xile@gmail.com>
2018-02-27 14:34:04 +0800

    4-3minus handle

--- Chapter 4/4-3/polishcalc.c
+++ Chapter 4/4-3/polishcalc.c
@@ -104,12 +104,19 @@ void ungetch(int);
 /* getop:  get next operator or numeric operand */
 int getop(char s[])
 {
-    int i, c;
+    int i, c, d;
 
     while ((s[0] = c = getch()) == ' ' || c == '\t');
     s[1] = '\0';
     if (!isdigit(c) && c != '.' && c != '-')
         return c;                       /* not a number */
+    if (c == '-') { /* handle minus */
+        d = getch();
+        if (!isdigit(d) && d != '.')
+            return c;
+        else 
+            ungetch(d);
+    }
     i = 0;
     if (isdigit(c) || c == '-')         /* collect integer part */
         while (isdigit(s[++i] = c = getch()));
